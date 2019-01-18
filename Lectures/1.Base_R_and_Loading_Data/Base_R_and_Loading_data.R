111d6cfe613c2012df2192ecce87b39326a35e59
DrewWham <fcw5014@psu.edu>
2019-01-18 09:41:01 -0500

    update to lecture 1

--- Lectures/1.Base_R_and_Loading_Data/Base_R_and_Loading_data.R
+++ Lectures/1.Base_R_and_Loading_Data/Base_R_and_Loading_data.R
@@ -34,6 +34,42 @@ DT1[1,]
 # returns the column named "V2", notice that this is a vector
 DT1[,V2]
 
+# you can also reference a column with the $ syntax
+
+DT1$V2
+
+# you can re-assighn values to a data.table
+
+DT1[1,2]<-FALSE
+
+# you can add new rows as long as they are correctly formatted
+
+new_row<-data.table(V1=10,V2=TRUE,V3="column")
+
+DT1<-rbind(DT1,new_row)
+
+# you can add a new column by giving a set of values to a column that does not exist
+
+DT1$V4<-c("G1","F1","G1","F1")
+
+# logical vectors can be converted to 0's and 1's
+DT1$V2<-DT1$V2*1
+
+# and then back again
+
+DT1$V2<-as.logical(DT1$V2)
+
+# There are other "as.'' functions
+
+DT1$V1<-as.character(DT1$V1)
+
+DT1$V4<-as.factor(DT1$V4)
+str(DT1)
+# and back again 
+
+DT1$V1<-as.numeric(DT1$V1)
+
+
 # ".csv" files are a common way to store data, we can load ".csv" files with the fread() function:
 # first, you will need to download the flights data
 source("./Lectures/Data/download_flights.R")
