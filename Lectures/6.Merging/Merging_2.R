235eccd3fbd044cd5c30732065120663d9c2ae02
Drew Wham <fcw5014@shields-117-fcw5014.local>
2019-02-22 09:49:25 -0500

    merging

--- Lectures/6.Merging/Merging_2.R
+++ Lectures/6.Merging/Merging_2.R
@@ -8,34 +8,25 @@ mat<-fread("./Lectures/Data/PUBG/match_stats.csv")
 #This reads in the data about airports and stores it as an object called 'AP'
 
 
-win_kills<-ind[killer_placement==1,c("killer_placement","match_id")]
 
-mat_win_kills<-dcast(win_kills,match_id~.,length,value.var=c("match_id"))
-
-setnames(mat_win_kills,".","winner_total_kills")
-
-mat_win_kills<-data.table(mat_win_kills)
-
-mat<-mat[,c("match_id","party_size","game_size")]
+mat<-mat[,c("match_id","party_size","game_size","player_kills")]
 
 mat<-mat[!duplicated(mat$match_id),]
 
-system.time(merge(mat_win_kills,mat,all.x=T))
+system.time(merge(ind,mat,all.x=T))
 
 setkey(mat,match_id)
-setkey(mat_win_kills,match_id)
+setkey(ind,match_id)
 
-system.time(merge(mat_win_kills,mat,all.x=T))
-
-
-
-mat_stats<-merge(mat_win_kills,mat,all.x=T)
+system.time(merge(ind,mat,all.x=T))
 
 
 
+mat_stats<-merge(ind,mat,all.x=T)
 
-dcast(mat_stats,party_size~.,mean,value.var=c("winner_total_kills"))
-dcast(mat_stats,party_size~game_size,length,value.var=c("winner_total_kills"))
+ggplot(mat_stats[killer_placement==1],aes(x=victim_placement))+geom_density()
+
+
 
 
 
