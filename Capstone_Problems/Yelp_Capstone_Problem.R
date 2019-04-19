2b7ff5e7882e6788ee4ad9d8681638964a4d5682
DrewWham <fcw5014@psu.edu>
2019-04-19 09:56:26 -0400

    capstone added

---
+++ Capstone_Problems/Yelp_Capstone_Problem.R
@@ -0,0 +1,19 @@
+#capstone problem 1
+
+# Using the Yelp review data, find the 5 businesses with the largest downward trend in reviews. 
+# Plot the businesses average review over time in a faceted plot. 
+# Select your own colors for the businesses plots using a manual color selection with hex values 
+# you gather from googles color picking tool.  
+
+
+# We will define "downward trend" as the business whos median change in month-over-month average reviews is lowest.
+# You should ignore any month for any business in which there were less than 30 reviews
+# you should ignore any business that does not have 5 or more valid months
+# once you have identified the 5 businesses be sure to include all of the data in your subsequent analysis
+
+
+
+# make a faceted barplot that shows the monthly average reviews over time for each business. each business should appear in its own facet.
+# give the business name at the top of each facet. Select a unique color for each business. Label each bar with
+# the number of reviews in that month. 
+
