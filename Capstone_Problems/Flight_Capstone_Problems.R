ed32ff7aca8b1ea475ea332bc39483dd229a94b2
DrewWham <fcw5014@psu.edu>
2019-04-26 09:53:09 -0400

    Flight Cap prob

---
+++ Capstone_Problems/Flight_Capstone_Problems.R
@@ -0,0 +1,41 @@
+#capstone problem set 2
+
+# All of these questions will reference the Flights dataset and will require both 
+# the "2008.csv" and "airports.csv" tables.
+
+#1
+# On average, flights that leave which airport travel the longest distance?
+
+#2
+# Delta, American Airlines and United are the primary carriers for passenger airlines out of the 
+# University Park Airport. Which of these airlines has the lowest average departure delay? 
+
+#3
+# Only considering Delta, American Airlines and United, in terms of departure delay what is the
+# best month for travel out of the University Park Airport?
+
+#4
+# Format the date and time as a date-time object. Then use geom_smooth to plot the average departure delay for Delta, American Airlines
+# and United across time. Select a unique color for each carrier. According to the geom_smooth plot, which carrier has the largest 
+# average delay in July? 
+
+#5
+# Re-plot the data over time of day. What is the relationship between time of day and average departure delay?
+
+
+#6 
+# Re-plot the data over the days of the week. What is the relationship between the day of the week and average departure delay?
+
+
+#7
+# Only considering Flights between SFO and ATL, what is the relationship between departure delay and airtime?
+
+#8
+# What type of delay is responsible for the largest amount of delay time for flights leaving O'Hare Int airport?
+
+#9
+# only considering flights that occure on the same day, if an airplane (TailNum) experiances a delay what
+# is the average delay time for that aircrafts next flight?
+
+# only considering flights that occure on the same day, if an airplane (TailNum) does not experiances a 
+# delay what is the average delay time for that aircrafts next flight?
