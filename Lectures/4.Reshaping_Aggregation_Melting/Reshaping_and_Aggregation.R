ab19298fcecbcd56d22a2880cb836cb901fcc7f8
DrewWham <Fcwham@gmail.com>
2019-05-28 09:24:21 -0400

    Update Reshaping_and_Aggregation.R

--- Lectures/4.Reshaping_Aggregation_Melting/Reshaping_and_Aggregation.R
+++ Lectures/4.Reshaping_Aggregation_Melting/Reshaping_and_Aggregation.R
@@ -4,41 +4,41 @@ library(stringr)
 library(lubridate)
 library(data.table)
 
+# extra reading and examples:
+# https://cloud.r-project.org/web/packages/data.table/vignettes/datatable-reshape.html
+
 
 #This reads in the flight data and stores it as an object called 'DT'
 DT<-fread("./Lectures/Data/Flights/2008.csv")
 #This reads in the data about airports and stores it as an object called 'AP'
 AP<-fread("./Lectures/Data/Flights/airports.csv")
 
-#there are several ways to subset
-
-#select all rows with vlaue 'IAD' as 'Origin'
-
-sub_IAD<-DT[Origin=='IAD',]
-
-#subset with a vector is also very useful
-#make a vector with all of the washington area airports
-WashAP<-c('DCA','IAD','BWI')
-
-WF<-DT[Origin %in% WashAP]
-
-dim(WF)
-
-WF<-WF[Cancelled==0]
 
 #Reshaping and aggregating are two of the most important functions you will use in data wrangling
 #We will use two methods
 
-#data.table has built in aggregation function
-WF[,.(Avg_Delay=mean(DepDelay)),by=Origin]
+#data.table has built in aggregation function that we have already used
+DT[,.(Avg_Delay=mean(DepDelay)),by=Origin]
 
 #dcast offeres a larger set of reshaping options
-Avg_Delay_tab<-dcast(WF,Origin ~ .,mean,value.var= c("DepDelay"))
+Avg_Delay_tab<-dcast(DT,Origin ~ .,mean,na.rm=T,value.var= c("DepDelay"))
+
+# dcast can scale your aggregations in many directions
+
 #dcast allows you to define multiple groupings
-Avg_Delay_tab<-dcast(WF,Origin ~ UniqueCarrier,mean,value.var= c("DepDelay"))
+Avg_Delay_tab<-dcast(DT,Origin ~ UniqueCarrier,mean,na.rm=T,value.var= c("DepDelay"))
+
+#dcast allows you calculate multiple summary stats
+Sum_Delay_tab<-dcast(DT,Origin ~ .,c(mean,sd),na.rm=T,value.var= c("DepDelay"))
+
+#dcast allows you calculate multiple summary stats on multiple columns 
+Sum_Delay_tab<-dcast(DT,Origin ~ .,c(mean,sd),na.rm=T,value.var= c("DepDelay", "ArrDelay"))
+
+#dcast allows you calculate multiple summary stats on multiple columns 
+Sum_Delay_Car_tab<-dcast(DT,Origin ~  UniqueCarrier,c(mean,sd),na.rm=T,value.var= c("DepDelay", "ArrDelay"))
 
 #this is the same information in tidy format
-Avg_Delay_tab<-dcast(WF,Origin + UniqueCarrier~.,mean,value.var= c("DepDelay"))
+Avg_Delay_tab<-dcast(DT,Origin + UniqueCarrier~.,mean,na.rm=T,value.var= c("DepDelay"))
 
 #rename the '.' column
 setnames(Avg_Delay_tab,".","Average_Delay")
