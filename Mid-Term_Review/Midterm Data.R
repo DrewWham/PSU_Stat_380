8b3965c2c74b6ee6d7f7b40a65dab6346d131a7c
Drew Wham <fcw5014@shields-117-fcw5014.local>
2019-06-03 09:52:05 -0400

    midterm data

---
+++ Mid-Term_Review/Midterm Data.R
@@ -0,0 +1,16 @@
+# This exam contains questions about an NCAA Mens basketball dataset. 
+# The dataset can be downloaded from the links below. Please note that 
+# you may be able to find some of the answers by searching the internet. 
+# In some instances I have changed this data so that information from this 
+# dataset does not reflect real life. So if you use the internet to search
+# for the answers rather than using the data supplied you do so at your own risk. 
+
+#The primary data can be found in the RegularSeasonHistory table. In this table
+# each row represents an outcome of a game for a team. Because each game results 
+# in 1 team winning and 1 team losing, there are 2 rows for each game in the dataset. 
+
+#https://s3.amazonaws.com/stat.184.data/NCAA/RegularSeasonHistory.csv (Links to an external site.)Links to an external site.
+
+#https://s3.amazonaws.com/stat.184.data/NCAA/TeamConferences.csv (Links to an external site.)Links to an external site.
+
+#https://s3.amazonaws.com/stat.184.data/NCAA/Teams.csv (Links to an external site.)Links to an external site.
