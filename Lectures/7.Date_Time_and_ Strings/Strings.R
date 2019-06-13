6e41c33e0949b2ba704ad811310ddeb1906d19fb
DrewWham <Fcwham@gmail.com>
2019-06-13 09:16:17 -0400

    Update Strings.R

--- Lectures/7.Date_Time_and_ Strings/Strings.R
+++ Lectures/7.Date_Time_and_ Strings/Strings.R
@@ -14,7 +14,7 @@ str(DT)
 # use gsub on bikeno 
 # https://www.cheatography.com/davechild/cheat-sheets/regular-expressions/
 
-DT$bike_num<-gsub("[0-9]", "", DT$bikeno) 
+DT$bike_num<-gsub("[^0-9]", "", DT$bikeno) 
 DT$bike_letter<-gsub("[0-9]", "", DT$bikeno) 
 
 # solution using stringi
@@ -34,7 +34,4 @@ Time<-data.table(matrix(unlist(strsplit(DT$sdate,"T")),ncol=2,byrow=T))
 # using stringi match
 
 sub_DT<-DT[stri_startswith_fixed(DT$sstation,"Wis")]
-
-# homework: make a bar plot of showing the number of rentals for each client type for each day of the week.
-# use a red for registered clients and green for casual clients. Each day of the week should be represented
-# on the x axis. Note that you will need to reshape the data in order to build this plot.  
+ 
