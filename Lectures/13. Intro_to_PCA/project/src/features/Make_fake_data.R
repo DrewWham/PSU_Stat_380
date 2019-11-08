b5daf839dd02f65d8de4725ef853ce5d74ca43c5
DrewWham <fcw5014@psu.edu>
2019-11-08 11:27:35 -0500

    update PCA lecture with tsne

--- Lectures/13. Intro_to_PCA/project/src/features/Make_fake_data.R
+++ Lectures/13. Intro_to_PCA/project/src/features/Make_fake_data.R
@@ -11,8 +11,13 @@ R2C<-c(0,1)
 GSHC<-c(0,1)
 Fox<-c(0,1)
 CNN<-c(0,1)
-
-
+Lower_Immigration<-c(0,1)
+Glob_W<-c(0,1)
+Tax_high_income<-c(0,1)
+Death_Pen<-c(0,1)
+Fed_too_much_Power<-c(0,1)
+Stricter_Gun_Laws<-c(0,1)
+Stricter_Env_laws<-c(0,1)
 
 
 R_Q1<-c(0.11,0.89) #https://news.gallup.com/poll/203198/presidential-approval-ratings-donald-trump.aspx
@@ -21,7 +26,13 @@ R_Q3<-c(0.65,0.35) #https://fivethirtyeight.com/features/the-abortion-debate-isn
 R_Q4<-c(0.88,0.12) #https://www.pewresearch.org/fact-tank/2018/10/03/most-continue-to-say-ensuring-health-care-coverage-is-governments-responsibility/
 R_Q5<-c(0.3,0.7) #https://www.businessinsider.com/most-and-least-trusted-news-outlets-in-america-cnn-fox-news-new-york-times-2019-4
 R_Q6<-c(0.7,0.3) #https://www.businessinsider.com/most-and-least-trusted-news-outlets-in-america-cnn-fox-news-new-york-times-2019-4
-
+R_Q7<-c(0.4,0.6) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+R_Q8<-c(0.6,0.4) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+R_Q9<-c(0.6,0.4) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+R_Q10<-c(0.2,0.8) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+R_Q11<-c(0.18,0.82) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+R_Q12<-c(0.66,0.34) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+R_Q13<-c(0.65,0.35) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
 
 D_Q1<-c(0.93,0.07) #https://news.gallup.com/poll/203198/presidential-approval-ratings-donald-trump.aspx
 D_Q2<-c(0.05,0.95) #https://www.washingtonpost.com/politics/americans-of-both-parties-overwhelmingly-support-red-flag-laws-expanded-gun-background-checks-washington-post-abc-news-poll-finds/2019/09/08/97208916-ca75-11e9-a4f3-c081a126de70_story.html
@@ -29,11 +40,17 @@ D_Q3<-c(0.25,0.75) #https://fivethirtyeight.com/features/the-abortion-debate-isn
 D_Q4<-c(0.49,0.51) #https://www.pewresearch.org/fact-tank/2018/10/03/most-continue-to-say-ensuring-health-care-coverage-is-governments-responsibility/
 D_Q5<-c(0.58,0.42) #https://www.businessinsider.com/most-and-least-trusted-news-outlets-in-america-cnn-fox-news-new-york-times-2019-4
 D_Q6<-c(0.18,0.82) #https://www.businessinsider.com/most-and-least-trusted-news-outlets-in-america-cnn-fox-news-new-york-times-2019-4
+D_Q7<-c(0.8,0.2) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+D_Q8<-c(0.11,0.89) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+D_Q9<-c(0.18,0.82) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+D_Q10<-c(0.59,0.41) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+D_Q11<-c(0.64,0.36) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+D_Q12<-c(0.23,0.77) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
+D_Q13<-c(0.21,0.79) #https://news.gallup.com/opinion/polling-matters/215210/partisan-differences-growing-number-issues.aspx
 
 
 
-
-Qs<-c("Ap_Pres","UBC","R2C","GSHC","Fox","CNN")
+Qs<-c("Ap_Pres","UBC","R2C","GSHC","Fox","CNN","Lower_Immigration","Glob_W","Tax_high_income","Death_Pen","Fed_too_much_Power","Stricter_Gun_Laws","Stricter_Env_laws")
 party<-c("R","D")
 
 
@@ -42,7 +59,7 @@ q_data<-NULL
 for (j in 1:length(party)){
   sp_tab<-NULL
   for (i in 1:length(Qs)){
-    msats<-sample(get(Qs[i]),2500,replace=T,prob=get(paste0(party[j],"_Q",i)))
+    msats<-sample(get(Qs[i]),5000,replace=T,prob=get(paste0(party[j],"_Q",i)))
     sp_tab<-cbind(sp_tab,msats)
     sp_tab<-data.table(sp_tab)
     names(sp_tab)[i]<-Qs[i]
@@ -58,7 +75,7 @@ q_data$Knitting<-0
 q_data[Cats==1]$Knitting<-sample(c(0,1),dim(q_data[Cats==1])[1],replace=T,prob=c(0.15,0.85))
 q_data[Cats==0]$Knitting<-sample(c(0,1),dim(q_data[Cats==0])[1],replace=T,prob=c(0.85,0.15))
 
-q_data<-q_data[,.(party,Ap_Pres,UBC,R2C,GSHC,Fox,CNN,Cats,Knitting)]
+q_data<-q_data[,.(party,Ap_Pres,UBC,R2C,GSHC,Fox,CNN,Lower_Immigration,Glob_W,Tax_high_income,Death_Pen,Fed_too_much_Power,Stricter_Gun_Laws,Stricter_Env_laws,Cats,Knitting)]
 
 fwrite(q_data,"./project/volume/data/raw/data.csv")
 
--- Lectures/13. Intro_to_PCA/project/src/models/PCA.R
+++ Lectures/13. Intro_to_PCA/project/src/models/PCA.R
@@ -12,8 +12,10 @@ data<-fread("./project/volume/data/raw/data.csv")
 party<-data$party
 data$party<-NULL
 
+j_data<-data.frame(lapply(data, jitter,factor=0.01))
+
 # do a pca
-pca<-prcomp(data)
+pca<-prcomp(j_data)
 
 # look at the percent variance explained by each pca
 screeplot(pca)
@@ -34,4 +36,23 @@ pca_dt<-data.table(unclass(pca)$x)
 pca_dt$party<-party
 
 # see a plot with the party data 
-ggplot(pca_dt,aes(x=PC1,y=PC4,col=party))+geom_point()
+ggplot(pca_dt,aes(x=PC1,y=PC2,col=party))+geom_point()
+
+
+
+
+# run t-sne on the PCAs, note that if you already have PCAs you need to set pca=F or it will run a pca again. 
+# pca is built into Rtsne, ive run it seperatly for you to see the internal steps
+
+tsne<-Rtsne(pca_dt,pca = F)
+
+# grab out the coordinates
+tsne_dt<-data.table(tsne$Y)
+
+# add back in party and cats so we can see what the analysis did with them
+tsne_dt$party<-party
+tsne_dt$Cats<-data$Cats
+
+# plot, note that in this case I have access to party so I can see that it seems to have worked, You do not have access
+# to species so you will just be plotting in black to see if there are groups. 
+ggplot(tsne_dt,aes(x=V1,y=V2,col=party))+geom_point()
