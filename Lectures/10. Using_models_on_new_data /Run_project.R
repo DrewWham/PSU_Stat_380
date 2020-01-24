7f8a9aacf87e359a40d650b10bcb33a64a6e3a76
DrewWham <fcwham@gmail.com>
2020-01-24 12:28:59 -0500

    remove lect 10

--- Lectures/10. Using_models_on_new_data /Run_project.R
+++
@@ -1,2 +0,0 @@
-source("./project/src/features/feature_eng_v1.R")
-source("./project/src/models/Simple_line.R")
--- Lectures/10. Using_models_on_new_data /project/required/requirements.R
+++
@@ -1,5 +0,0 @@
-install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("Metrics",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("caret",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-
-
