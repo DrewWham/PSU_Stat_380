7bd7910b4eea113e64a360f81302fabfea90afee
DrewWham <fcwham@gmail.com>
2020-01-24 12:27:00 -0500

    remove space

---
+++ Lectures/10. Using_models_on_new_data/project/required/requirements (1).R
@@ -0,0 +1,6 @@
+install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("Metrics",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("caret",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("glmnet",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("plotmo",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+
