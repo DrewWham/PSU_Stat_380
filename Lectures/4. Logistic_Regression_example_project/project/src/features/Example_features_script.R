53a813c781ef05826d231b26b81c53a3f0c5045d
DrewWham <fcwham@gmail.com>
2020-02-05 12:18:12 -0500

    split on year

--- Lectures/4. Logistic_Regression_example_project/project/src/features/Example_features_script.R
+++ Lectures/4. Logistic_Regression_example_project/project/src/features/Example_features_script.R
@@ -5,17 +5,19 @@ set.seed(77)
 market_DT<-data.table(Smarket)
 
 market_DT$Today<-NULL
-market_DT$Year<-NULL
+
 
 market_DT$Direction<-1*(market_DT$Direction=="Up")
 
 
 # here I divide the data into train and test so that I'm working on a similar problem as all of you
 # note that you do not need to do this on your dataset
-rand_inx<-sample(1:nrow(market_DT),250)
 
-train<-market_DT[!rand_inx,]
-test<-market_DT[rand_inx,]
+train<-market_DT[!Year==2005,]
+test<-market_DT[Year==2005,]
+
+train$Year<-NULL
+test$Year<-NULL
 
 fwrite(train,'./project/volume/data/interim/train.csv')
 fwrite(test,'./project/volume/data/interim/test.csv')
