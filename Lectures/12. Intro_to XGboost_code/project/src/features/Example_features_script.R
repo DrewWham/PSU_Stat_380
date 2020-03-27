45cc2783a2766959ee224d9aa2f0ebf648d5163d
DrewWham <fcwham@gmail.com>
2020-03-26 22:50:41 -0400

    update xgboost code

---
+++ Lectures/12. Intro_to XGboost_code/project/src/features/Example_features_script.R
@@ -0,0 +1,21 @@
+library(data.table)
+set.seed(77)
+
+DT<-fread('./project/volume/data/raw/2008.csv')
+
+pairs(DepDelay~CRSDepTime+Distance,data=DT[1:1000,])
+
+DT[is.na(DT$DepDelay)]$DepDelay<-0
+
+sub_DT<-DT[,.(UniqueCarrier,CRSDepTime,Distance,DepDelay)]
+
+# here I divide the data into train and test so that I'm working on a similar problem as all of you
+# note that you do not need to do this on your dataset
+rand_inx<-sample(1:nrow(sub_DT),1000000)
+
+train<-sub_DT[!rand_inx,]
+test<-sub_DT[rand_inx,]
+
+fwrite(train,'./project/volume/data/interim/train.csv')
+fwrite(test,'./project/volume/data/interim/test.csv')
+
