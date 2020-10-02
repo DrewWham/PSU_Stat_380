9555636bc2fef188b1c624eb73df420c4671e9c3
DrewWham <fcwham@gmail.com>
2020-10-02 13:28:30 -0400

    update oct 2

--- Lectures/6. Feature_engineering_example_project/project/src/models/glmnet_model.R
+++ Lectures/6. Feature_engineering_example_project/project/src/models/glmnet_model.R
@@ -8,8 +8,8 @@ library(lubridate)
 
 
 #read in data, notice the path will always look like this because the assumed working directory is the repo level folder
-train<-fread("./project/volume/data/interim/train_v1.csv")
-test<-fread("./project/volume/data/interim/test_v1.csv")
+train<-fread("./project/volume/data/interim/train_bs.csv")
+test<-fread("./project/volume/data/interim/test_bs.csv")
 example_sub<-fread("./project/volume/data/raw/example_submission.csv")
 
 ##########################
@@ -28,6 +28,11 @@ drops<- c('id','future_date','current_date')
 train<-train[, !drops, with = FALSE]
 test<-test[, !drops, with = FALSE]
 
+
+#keep<-c("current_price","future_price","rarity","Land","BS_199","BS_200","BS_201")
+
+#train<-train[, keep, with = FALSE]
+#test<-test[, keep, with = FALSE]
 
 #save the response var because dummyVars will remove
 train_y<-train$future_price
