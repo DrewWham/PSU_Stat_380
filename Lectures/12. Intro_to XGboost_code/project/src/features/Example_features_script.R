b7e1fa6b784dcc5ebc3e3ffa43a61e8792e25a27
DrewWham <fcwham@gmail.com>
2020-10-19 12:55:54 -0400

    xgboost update

--- Lectures/12. Intro_to XGboost_code/project/src/features/Example_features_script.R
+++ Lectures/12. Intro_to XGboost_code/project/src/features/Example_features_script.R
@@ -16,6 +16,6 @@ rand_inx<-sample(1:nrow(sub_DT),1000000)
 train<-sub_DT[!rand_inx,]
 test<-sub_DT[rand_inx,]
 
-fwrite(train,'./project/volume/data/interim/train.csv')
+fwrite(train[1:100000,],'./project/volume/data/interim/train.csv')
 fwrite(test,'./project/volume/data/interim/test.csv')
 
