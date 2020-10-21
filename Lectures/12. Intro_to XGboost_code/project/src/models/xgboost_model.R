c0af8da343a1833ab22c96c2c6ab6d4c1712f0a7
DrewWham <fcwham@gmail.com>
2020-10-21 14:13:11 -0400

    add hyper param tune table

--- Lectures/12. Intro_to XGboost_code/project/src/models/xgboost_model.R
+++ Lectures/12. Intro_to XGboost_code/project/src/models/xgboost_model.R
@@ -33,7 +33,7 @@ x.test<-predict(dummies, newdata = test)
 dtrain <- xgb.DMatrix(x.train,label=y.train,missing=NA)
 dtest <- xgb.DMatrix(x.test,missing=NA)
 
-
+hyper_perm_tune<-NULL
 ########################
 # Use cross validation #
 ########################
@@ -42,8 +42,8 @@ param <- list(  objective           = "reg:linear",
                 gamma               =0.02,
                 booster             = "gbtree",
                 eval_metric         = "rmse",
-                eta                 = 0.002,
-                max_depth           = 20,
+                eta                 = 0.02,
+                max_depth           = 5,
                 min_child_weight    = 1,
                 subsample           = 1.0,
                 colsample_bytree    = 1.0,
@@ -51,8 +51,17 @@ param <- list(  objective           = "reg:linear",
 )
 
 
-XGBm<-xgb.cv( params=param,nfold=5,nrounds=1000,missing=NA,data=dtrain,print_every_n=1)
+XGBm<-xgb.cv( params=param,nfold=5,nrounds=10000,missing=NA,data=dtrain,print_every_n=1,early_stopping_rounds=25)
 
+best_ntrees<-unclass(XGBm)$best_iteration
+
+new_row<-data.table(t(param))
+
+new_row$best_ntrees<-best_ntrees
+
+test_error<-unclass(XGBm)$evaluation_log[best_ntrees,]$test_rmse_mean
+new_row$test_error<-test_error
+hyper_perm_tune<-rbind(new_row,hyper_perm_tune)
 
 ####################################
 # fit the model to all of the data #
@@ -68,7 +77,8 @@ watchlist <- list( train = dtrain)
 # you need an evaluation set for that, you do not have that available to you for the house data. You also should have 
 # figured out the number of trees (nrounds) from the cross validation step above. 
 
-XGBm<-xgb.train( params=param,nrounds=400,missing=NA,data=dtrain,watchlist=watchlist,print_every_n=1)
+XGBm<-xgb.train( params=param,nrounds=best_ntrees,missing=NA,data=dtrain,watchlist=watchlist,print_every_n=1)
+
 
 # just like the other model fitting methods we have seen, we can use the predict function to get predictions from the 
 # model object as long as the new data is identical in format to the training data. Note that this code saves the
