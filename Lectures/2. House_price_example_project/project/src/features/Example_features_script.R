24c6af011859e7ec14d5d77df05bc9dac04631d6
DrewWham <Fcwham@gmail.com>
2022-01-20 13:36:42 -0500

    Update Example_features_script.R

--- Lectures/2. House_price_example_project/project/src/features/Example_features_script.R
+++ Lectures/2. House_price_example_project/project/src/features/Example_features_script.R
@@ -3,7 +3,7 @@ set.seed(77)
 
 DT<-fread('./project/volume/data/raw/2008.csv')
 
-sub_DT<-DT[!is.na(DT$DepDelay)][,.(DepTime,DepDelay,UniqueCarrier)]
+sub_DT<-DT[!is.na(DT$DepDelay)][,.(UniqueCarrier,Origin,DepDelay)]
 
 # here I divide the data into train and test so that I'm working on a similar problem as all of you
 # note that you do not need to do this on your dataset
@@ -12,6 +12,12 @@ rand_inx<-sample(1:nrow(sub_DT),1000000)
 train<-sub_DT[!rand_inx,]
 test<-sub_DT[rand_inx,]
 
-fwrite(train,'./project/volume/data/interim/train.csv')
-fwrite(test,'./project/volume/data/interim/test.csv')
+train$id<-1:nrow(train)
+test$id<-1:nrow(test)
 
+fwrite(train,'./project/volume/data/interim/train.csv')
+fwrite(test[,.(id,DepDelay)],'./project/volume/data/interim/solution.csv')
+fwrite(test[,.(id,UniqueCarrier, Origin)],'./project/volume/data/interim/test.csv')
+
+test$DepDelay<-1
+fwrite(test[,.(id,DepDelay)],'./project/volume/data/interim/example_sub.csv')
