366838baaf738f9b02d088977c01cf482c2fb221
DrewWham <Fcwham@gmail.com>
2023-08-31 13:14:51 -0400

    Create Make_Classroom_Dataset.R

---
+++ Lectures/2. House_price_example_project/project/src/features/Make_Classroom_Dataset.R
@@ -0,0 +1,24 @@
+library(data.table)
+set.seed(77)
+
+DT<-fread('./project/volume/data/raw/2008.csv')
+
+sub_DT<-DT[!is.na(DT$DepDelay)][,.(UniqueCarrier,Origin,DepDelay)]
+
+# here I divide the data into train and test so that I'm working on a similar problem as all of you
+# note that you do not need to do this on your dataset
+rand_inx<-sample(1:nrow(sub_DT),1000000)
+
+train<-sub_DT[!rand_inx,]
+test<-sub_DT[rand_inx,]
+
+train$id<-1:nrow(train)
+test$id<-1:nrow(test)
+
+train$id<-paste0("train_",train$id)
+test$id<-paste0("test_",test$id)
+
+fwrite(train[,.(id,UniqueCarrier, Origin,DepDelay)],'./project/volume/data/raw/train.csv')
+fwrite(test[,.(id,DepDelay)],'./project/volume/data/raw/solution.csv')
+test$DepDelay<-1
+fwrite(test[,.(id,UniqueCarrier, Origin,DepDelay)],'./project/volume/data/raw/test.csv')
