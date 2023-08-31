04d4ef7b743215756e9efae8d8568c8f73c90b74
DrewWham <Fcwham@gmail.com>
2023-08-31 13:14:02 -0400

    Update Example_features_script.R

--- Lectures/2. House_price_example_project/project/src/features/Example_features_script.R
+++ Lectures/2. House_price_example_project/project/src/features/Example_features_script.R
@@ -1,23 +1,7 @@
 library(data.table)
-set.seed(77)
+library(Metrics)
 
-DT<-fread('./project/volume/data/raw/2008.csv')
 
-sub_DT<-DT[!is.na(DT$DepDelay)][,.(UniqueCarrier,Origin,DepDelay)]
-
-# here I divide the data into train and test so that I'm working on a similar problem as all of you
-# note that you do not need to do this on your dataset
-rand_inx<-sample(1:nrow(sub_DT),1000000)
-
-train<-sub_DT[!rand_inx,]
-test<-sub_DT[rand_inx,]
-
-train$id<-1:nrow(train)
-test$id<-1:nrow(test)
-
-fwrite(train,'./project/volume/data/interim/train.csv')
-fwrite(test[,.(id,DepDelay)],'./project/volume/data/interim/solution.csv')
-fwrite(test[,.(id,UniqueCarrier, Origin)],'./project/volume/data/interim/test.csv')
-
-test$DepDelay<-1
-fwrite(test[,.(id,DepDelay)],'./project/volume/data/interim/example_sub.csv')
+train<-fread('./project/volume/data/raw/train.csv')
+test<-fread('./project/volume/data/raw/test.csv')
+solution<-fread('./project/volume/data/raw/solution.csv')
