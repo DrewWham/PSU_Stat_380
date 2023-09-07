253ee2021d05e2dd90e1a45bc95d80fe7ed0ee4d
DrewWham <Fcwham@gmail.com>
2023-09-07 13:21:26 -0400

    Update Example_features_script.R

--- Lectures/3. Linear_Regression_example_project/project/src/features/Example_features_script.R
+++ Lectures/3. Linear_Regression_example_project/project/src/features/Example_features_script.R
@@ -1,22 +1,11 @@
-library(data.table)
 set.seed(77)
 
-#you will need to place the data into the "./project/volume/data/raw/" folder
-DT<-fread('./project/volume/data/raw/2008.csv')
+train<-fread('./project/volume/data/raw/train.csv')
+test<-fread('./project/volume/data/raw/test.csv')
 
-pairs(DepDelay~CRSDepTime+DayofMonth,data=DT[1:1000,])
+train<-train[,.(DepTime,Distance,UniqueCarrier,DepDelay)]
+test<-test[,.(DepTime,UniqueCarrier,Distance)]
 
-DT[is.na(DT$DepDelay)]$DepDelay<-0
-
-sub_DT<-DT[,.(UniqueCarrier,Origin,CRSDepTime,Distance,DepDelay)]
-
-# here I divide the data into train and test so that I'm working on a similar problem as all of you
-# note that you do not need to do this on your dataset
-rand_inx<-sample(1:nrow(sub_DT),1000000)
-
-train<-sub_DT[!rand_inx,]
-test<-sub_DT[rand_inx,]
-
-fwrite(train,'./project/volume/data/interim/train.csv')
-fwrite(test,'./project/volume/data/interim/test.csv')
+fwrite(train,"./project/volume/data/interim/train.csv")
+fwrite(test,"./project/volume/data/interim/test.csv")
 
