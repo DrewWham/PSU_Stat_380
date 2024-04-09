65d35813b659f7724deb7f68fded987faea264c9
DrewWham <Fcwham@gmail.com>
2024-04-09 09:50:21 -0400

    Update DNN_embeddings.R

--- Lectures/14. Final_Project_Using_Embeddings/project/src/models/DNN_embeddings.R
+++ Lectures/14. Final_Project_Using_Embeddings/project/src/models/DNN_embeddings.R
@@ -3,6 +3,7 @@ library(data.table)
 library(Rtsne)
 library(ggplot2)
 library(reticulate)
+source("./project/src/models/cosine.R")
 
 tf<-import("tensorflow")
 hub<-import("tensorflow_hub")
@@ -11,7 +12,7 @@ np<-import("numpy")
 module_url <- "https://tfhub.dev/google/universal-sentence-encoder/4"
 model = hub$load(module_url)
 
-set.seed(3)
+set.seed(21)
 
 
 getEmbeddings<-function(text){
@@ -32,7 +33,7 @@ for (i in 1:length(data$text)){
 }
 emb_dt<-data.table(emb_dt)
 
-tsne<-Rtsne(emb_dt,perplexity=5)
+tsne<-Rtsne(emb_dt,perplexity=10)
 
 tsne_dt<-data.table(tsne$Y)
 
@@ -41,3 +42,15 @@ tsne_dt$id<-data$PSU_access_id
 
 ggplot(tsne_dt,aes(x=V1,y=V2,col=pet,label=id))+geom_text()
 
+
+
+neural_search<-function(new_text,n){
+  new_emb<-getEmbeddings(new_text)
+  data$cos_dist<-apply(emb_dt,1,function(x){return(cosine(as.numeric(new_emb),as.numeric(x)))})
+  out<-data[order(-cos_dist)][1:n,]
+  out
+}
+
+new_text<-"I have a tiny old dog that likes me to take him places."
+
+neural_search(new_text,5)
