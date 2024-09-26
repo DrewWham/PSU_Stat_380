f5c0c9d3f97750755f38eaa62ef7b61b97015d55
DrewWham <Fcwham@gmail.com>
2024-09-26 12:46:24 -0400

    Update Example_features_script.R

--- Lectures/3. Linear_Regression_example_project/project/src/features/Example_features_script.R
+++ Lectures/3. Linear_Regression_example_project/project/src/features/Example_features_script.R
@@ -1,10 +1,36 @@
+library(data.table)
+library(lubridate)
+
 set.seed(77)
 
 train<-fread('./project/volume/data/raw/train.csv')
 test<-fread('./project/volume/data/raw/test.csv')
 
-train<-train[,.(DepTime,Distance,UniqueCarrier,DepDelay)]
-test<-test[,.(DepTime,UniqueCarrier,Distance)]
+train$train<-1
+test$train<-0
+
+master<-rbind(train,test)
+
+
+# Convert the numeric time into a zero-padded string of length 4
+master$DepTime <- sprintf("%04d", master$DepTime)
+  
+# Parse time as "hour:minute" using lubridate::hm
+master$hour <- as.numeric(substr(master$DepTime, 1, 2))
+master$minute<- as.numeric(substr(master$DepTime, 3, 4))
+    
+  
+# Calculate total minutes past midnight
+master$Dept_minute <- (master$hour * 60) + master$minute
+  
+ 
+train<-master[train==1]
+test<-master[train==0]
+
+
+
+train<-train[,.(Dept_minute,Distance,UniqueCarrier,DepDelay)]
+test<-test[,.(Dept_minute,Distance,UniqueCarrier)]
 
 fwrite(train,"./project/volume/data/interim/train.csv")
 fwrite(test,"./project/volume/data/interim/test.csv")
